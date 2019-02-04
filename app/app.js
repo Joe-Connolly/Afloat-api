ae08efd094f55a3a84deed9501e54f09fdf2fc5a
Tyler <tyler@burnamtech.com>
2019-02-04 15:43:55 -0500

    Updated file structure, added middleware handlers to express

--- app/app.js
+++ app/app.js
@@ -1,9 +1,45 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+import express from 'express';
+import compression from 'compression';
+import helmet from 'helmet';
+import cors from 'cors';
+import morgan from 'morgan';
+import requestIp from 'request-ip';
+import routes from './rest';
+import chalk from 'chalk';
 
-var app = express();
+/**
+ * @name init
+ */
+const HOST = '0.0.0.0';
+const PORT = 3000;
+const app = express();
 
-app.listen(3000, function(){
-  console.log("Server started now");
-})
+/**
+ * @name middleware-functions
+ */
+app.use(compression());
+app.use(helmet());
+app.use(cors());
+
+// Get IPs
+app.use(requestIp.mw());
+app.set('trust proxy', true);
+
+app.use(morgan('tiny'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+/**
+ * @name REST
+ */
+app.use('/api', routes);
+
+
+/**
+ * @name api-server
+ */
+const server = app.listen(Number(PORT), HOST, () => {
+  console.log(chalk.hex('#009688')(` [*] Host: http://${HOST}:${PORT}/.`));
+});
+
+export default server;
