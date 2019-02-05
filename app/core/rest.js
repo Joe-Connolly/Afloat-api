a44789b4689355a8f7c93e87b2bf0ace33ab6c3c
Tyler <tyler@burnamtech.com>
2019-02-04 20:24:20 -0500

    added githook verification

--- app/core/rest.js
+++ app/core/rest.js
@@ -23,10 +23,11 @@ router.post('/getGitUpdate', (req, res) => {
   console.log(req.headers);
   console.log();
   console.log();
-  console.log(JSON.stringify(req.body));
-  if (req.body.payload.action === 'closed') {
-    console.log('Git pull and reset the server');
-  }
+  // console.log(JSON.stringify(req.body));
+  // if (req.body.payload.action === 'closed') {
+  //   console.log('Git pull and reset the server');
+  // }
+  res.send('success');
 });
 
 // Test restart
