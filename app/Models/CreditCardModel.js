1d0fb9a9d2668fd4bd7994a0933212bd6d52a634
Azhar Hussain <azhar.m.hussain.19@dartmouth.edu>
2019-02-06 13:49:36 -0500

    add CreditCard Model

---
+++ app/Models/CreditCardModel.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from 'mongoose';
+
+// Base credit card schema
+const CreditCardSchema = new Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  number: Number,
+  name: String,
+  exp: Number,
+  address: String,
+});
+
+CreditCardSchema.set('toJSON', {
+  virtuals: true,
+});
+
+// Pre-save hook left in case we want to validate card number before saving
+// CreditCardSchema.pre('save', function beforeUserSave(next) {
+//
+// });
+
+// Create model class
+const User = mongoose.model('User', CreditCardSchema);
+
+export default User;
