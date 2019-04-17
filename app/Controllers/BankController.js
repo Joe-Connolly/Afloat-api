64ce70ccbb98971d042dd0a6e612bbf9da33948e
Azhar Hussain <azhar.m.hussain.19@dartmouth.edu>
2019-04-16 21:23:11 -0400

    adding ACH transfer

--- app/Controllers/BankController.js
+++ app/Controllers/BankController.js
@@ -1,8 +1,14 @@
 import mongoose from 'mongoose';
 import plaid from 'plaid';
+import dwolla from 'dwolla-v2';
 
 import User from '../Models/UserModel';
 
+const plaidClientId = '5ca67be24a965e00118c857b';
+const plaidSecret = 'f19c9bfdf87541cf00b69428ea2113';
+const plaidPublic = 'beae29cb016901b1a1d8ac65538e8a';
+const dwallaPublic = 'UWHnZ6XbTZ5bZV0nVbgAZLlkOUCsY8seTSzmrul6oKyExU73C6';
+const dwallaKey = 'gfqUFFSAAU61tLhlriKIyysvau5nsFZLrDIdf3saDuGKF1Av6i';
 
 export const addBank = (req, res) => {
   User.findById(
@@ -12,7 +18,7 @@ export const addBank = (req, res) => {
         res.status(422).send({ err });
       }
 
-      const plaidClient = new plaid.Client('5ca67be24a965e00118c857b', 'f19c9bfdf87541cf00b69428ea2113', 'beae29cb016901b1a1d8ac65538e8a', plaid.environments.sandbox);
+      const plaidClient = new plaid.Client(plaidClientId, plaidSecret, plaidPublic, plaid.environments.sandbox);
       // console.log(req.body.publicToken);
       // createPublicToken(String, Function)
 
@@ -21,14 +27,112 @@ export const addBank = (req, res) => {
           user.publicToken = req.body.publicToken;
           user.accessToken = response.access_token;
           user.itemId = response.item_id;
+          console.log('Account ID');
+          console.log(req.body.accountId);
+          user.accountId = req.body.accountId;
           user.bankSet = true;
-          console.error('loaded');
-          user.save((e) => {
-            if (e) {
-              console.error('ERROR!');
-            }
+          console.error(response);
+
+
+          const client = new dwolla.Client({
+            key: dwallaPublic,
+            secret: dwallaKey,
+            environment: 'sandbox',
           });
+
+          const requestBody = {
+            firstName: user.firstname,
+            lastName: user.lastname,
+            email: user.email,
+          };
+
+          client.auth.client()
+            .then((appToken) => {
+              appToken.post('customers', requestBody)
+                .then((createCustomerResponse) => {
+                  const customerUrl = createCustomerResponse.headers.get('location');
+                  user.customerUrl = customerUrl;
+
+                  plaidClient.createProcessorToken(user.accessToken, user.accountId,
+                    'dwolla', (err2, res2) => {
+                      const processorToken = res2.processor_token;
+                      const requestBodyFunding = {
+                        plaidToken: processorToken,
+                        name: 'check',
+                      };
+                      appToken
+                        .post(`${customerUrl}/funding-sources`, requestBodyFunding)
+                        .then((res3) => {
+                          const userAccount = res3.headers.get('location');
+                          console.log('got in');
+                          console.log(userAccount);
+                          user.accountUrl = userAccount;
+                          user.save((e) => {
+                            if (e) {
+                              console.error('ERROR!');
+                            }
+                          });
+                          console.log(user);
+                          res.status(200);
+                        });
+                    });
+                }, (err3) => { console.log(err3); });
+            });
+
           res.status(200).send();
+        });
+    },
+  );
+};
+
+export const transferAchToUser = (req, res) => {
+  User.findById(
+    req.user.id,
+    (err, user) => {
+      if (err) {
+        res.status(422).send({ err });
+      }
+
+      const client = new dwolla.Client({
+        key: dwallaPublic,
+        secret: dwallaKey,
+        environment: 'sandbox',
+      });
+
+      client.auth.client()
+        .then((appToken) => {
+          const requestBody = {
+            _links: {
+              source: {
+                href: 'https://api-sandbox.dwolla.com/funding-sources/59e9024d-b406-4a48-9c9a-7d019a7d242a',
+              },
+              destination: {
+                href: user.accountUrl,
+              },
+            },
+            amount: {
+              currency: 'USD',
+              value: '100.00',
+            },
+            metadata: {
+              paymentId: '12345678',
+              note: 'test payment',
+            },
+            clearing: {
+              destination: 'next-available',
+              source: 'standard',
+            },
+            correlationId: '8a2cdc8d-629d-4a24-98ac-40b735229fe2',
+          };
+
+          appToken
+            .post('transfers', requestBody)
+            .then((res3) => {
+              const transfer = res3.headers.get('location');
+              console.log('transfered');
+              console.log(transfer);
+              res.send({ transfer });
+            });
         });
     },
   );
@@ -41,17 +145,13 @@ export const getTransactions = (req, res) => {
       if (err) {
         res.status(422).send({ err });
       }
-      const plaidClient = new plaid.Client('5ca67be24a965e00118c857b', 'f19c9bfdf87541cf00b69428ea2113', 'beae29cb016901b1a1d8ac65538e8a', plaid.environments.sandbox);
-      plaidClient.getTransactions(user.accessToken, '2018-01-01', '2019-04-13').then((resAccess) => {
+      const plaidClient = new plaid.Client(plaidClientId, plaidSecret, plaidPublic, plaid.environments.sandbox);
+      plaidClient.getTransactions(user.accessToken, '2019-01-01', '2019-04-13').then((resAccess) => {
         res.send(resAccess);
       }).catch((error) => { console.log(error); });
     },
   );
 };
-
-// plaidClient.getTransactions(accessToken, '2018-01-01', '2019-03-01').then((resAccess) => {
-//   console.log('hi');
-// }).catch((error) => { console.log(error); });
 
 export const getCardsForUser = (req, res) => {
   CreditCard.find({ user: req.user.id }, (err, docs) => {
